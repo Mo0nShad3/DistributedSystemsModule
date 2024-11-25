b1539467c0957dbcb63b215c2425cdc7b008681f
leonjroper <leonjroper@gmail.com>
2024-11-25 11:45:50 +0000

    Main chunk of Server/Admin GUI and Client GUI

--- Client/GUI.py
+++ Client/GUI.py
@@ -1,50 +1,92 @@
 import customtkinter
 import random
+from datetime import datetime, timedelta
 
-date = "21.10.24"
-time = "14:34"
+user = "Customer"
 
-price = "£24.05"
+ # STATUS NEEDS ADMIN OR BACKEND INTEGRATION
+ # |
+ # v
+
+status = True
+
+date_and_time = datetime.now()
+time = date_and_time.strftime("%H:%M")
+date = date_and_time.strftime("%d/%m/%Y")
+
+def get_date (days_ago):
+    today = datetime.now()
+    target_date = today - timedelta(days=days_ago)
+    return target_date.strftime("%d/%m/%Y")
+
+ # PRICE NEEDS BACKEND INTEGRATION
+ # |
+ # v
+
+def get_price (days_ago_price):
+    return "£ " + str(days_ago_price) + "d ago"
+
+def get_status (boolean):
+    if boolean == True:
+        return "ONLINE"
+    else:
+        return "OFFLINE"
 
 class billLog_Frame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
         # Widgets
-        self.dateLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=date)
+        self.dateLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=get_date(4))
         self.dateLabel1.grid(row=0, column=0, padx=20, pady=(15,0), sticky="nw")
-        self.priceLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=price)
+        self.priceLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=get_price(4))
         self.priceLabel1.grid(row=1, column=0, padx=20, pady=(0, 15), sticky="sw")
 
-        self.dateLabel2 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=date)
+        self.dateLabel2 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=get_date(3))
         self.dateLabel2.grid(row=0, column=1, padx=20, pady=(15,0), sticky="nw")
-        self.priceLabel2 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=price)
+        self.priceLabel2 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=get_price(3))
         self.priceLabel2.grid(row=1, column=1, padx=20, pady=(0,15), sticky="sw")
 
-        self.dateLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=date)
-        self.dateLabel1.grid(row=0, column=2, padx=20, pady=(15,0), sticky="nw")
-        self.priceLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=price)
-        self.priceLabel1.grid(row=1, column=2, padx=20, pady=(0,15), sticky="sw")
+        self.dateLabel3 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=get_date(2))
+        self.dateLabel3.grid(row=0, column=2, padx=20, pady=(15,0), sticky="nw")
+        self.priceLabel3 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=get_price(2))
+        self.priceLabel3.grid(row=1, column=2, padx=20, pady=(0,15), sticky="sw")
 
-        self.dateLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=date)
-        self.dateLabel1.grid(row=0, column=3, padx=20, pady=(15,0), sticky="nw")
-        self.priceLabel1 = customtkinter.CTkLabel(self,  font=("Consolas" ,14), text_color="#636fe3", text=price)
-        self.priceLabel1.grid(row=1, column=3, padx=20, pady=(0,15), sticky="sw")
+        self.dateLabel4 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=get_date(1))
+        self.dateLabel4.grid(row=0, column=3, padx=20, pady=(15,0), sticky="nw")
+        self.priceLabel4 = customtkinter.CTkLabel(self,  font=("Consolas" ,14), text_color="#636fe3", text=get_price(1))
+        self.priceLabel4.grid(row=1, column=3, padx=20, pady=(0,15), sticky="sw")
 
-        self.dateLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=date)
-        self.dateLabel1.grid(row=0, column=4, padx=20, pady=(15,0), sticky="nw")
-        self.priceLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=price)
-        self.priceLabel1.grid(row=1, column=4, padx=20, pady=(0,15), sticky="sw")
+        self.dateLabel5 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text=get_date(0))
+        self.dateLabel5.grid(row=0, column=4, padx=20, pady=(15,0), sticky="nw")
+        self.priceLabel5 = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#636fe3", text=get_price(0))
+        self.priceLabel5.grid(row=1, column=4, padx=20, pady=(0,15), sticky="sw")
 
 class userCard_Frame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        # Widgets
-        self.loggedInLabel = customtkinter.CTkLabel(self, font=("Consolas" ,10), text_color="#A3A3A3", text="You are logged in as:")
-        self.loggedInLabel.grid(row=0, column=0, padx=20, pady=(15,0), sticky="nw")
-        self.nameLabel = customtkinter.CTkLabel(self, font=("Consolas" ,14), text_color="#FFFFF0", text="Temitope Adeosun")
-        self.nameLabel.grid(row=1, column=0, padx=20, pady=(0, 15), sticky="sw")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.loggedInLabel = customtkinter.CTkLabel(
+            self,
+            font=("Consolas", 10),
+            text_color="#A3A3A3",
+            text="You are logged in as:",
+            anchor="w"
+        )
+        self.loggedInLabel.grid(row=0, column=0, padx=20, pady=(15,0), sticky="nsew")
+
+        self.nameLabel = customtkinter.CTkLabel(
+            self,
+            font=("Consolas", 14),
+            text_color="#FFFFF0",
+            text=user,
+            anchor="w"
+        )
+        self.nameLabel.grid(row=1, column=0, padx=20, pady=(0, 15), sticky="nsew")
 
         #self.profilePicture = customtkinter.CTkImage("Icon2.ico")
         #self.profilePicture.grid(row=0, column=1, padx=20, pady=(15,0), sticky="e")
@@ -53,26 +95,57 @@ class status_Frame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        # Widgets
-        self.statusLabel = customtkinter.CTkLabel(self, font=("Consolas" ,10), text_color="#FFFFF0", text="Powergrid Status: ONLINE")
-        self.statusLabel.grid(row=0, column=0, padx=20, pady=(15,0), sticky="nw")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.statusLabel = customtkinter.CTkLabel(
+            self,
+            font=("Consolas", 12),
+            text_color="#FFFFF0",
+            text="Powergrid Status: " + get_status(status),
+            anchor="w"
+        )
+        self.statusLabel.grid(row=0, column=0, padx=20, pady=(10, 10), sticky="nsew")
+
+        self.update_status()
+
+    def update_status(self):
+        self.statusLabel.configure(text="Powergrid Status: " + get_status(status))
+        self.after(1000, self.update_status)
+
 
 class timeCard_Frame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        # Widgets
-        self.timeLabel = customtkinter.CTkLabel(self, font=("Consolas" ,40), text_color="#FFFFF0", text=time)
-        self.timeLabel.grid(row=0, column=0, padx=(10,50), pady=40, sticky="nsew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.timeLabel = customtkinter.CTkLabel(
+            self,
+            font=("Helvetica", 60),
+            text_color="#555555",
+            text="",
+            anchor="center"
+        )
+        self.timeLabel.grid(row=0, column=0, padx=(10, 10), pady=20, sticky="nsew")
+
+        self.update_time()
+
+    def update_time(self):
+        current_time = datetime.now().strftime("%H:%M")
+        self.timeLabel.configure(text=current_time)
+        self.after(1000, self.update_time)
 
 class mainWindow(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.geometry("616x254")
-        self.title("Smart Meter")
+        self.title("Smart Meter: User")
         #self.wm_iconbitmap("Icon2.ico")
         self.grid_columnconfigure((0,1), weight=1)
         self.grid_rowconfigure((0,1), weight=1)
+        self.resizable(False, False)
 
         # Add widgets to app
         # ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
@@ -80,10 +153,10 @@ class mainWindow(customtkinter.CTk):
         self.billLog.grid(row=0, column=0, padx=10, pady=(10,0), sticky="nsew", columnspan=2)
 
         self.userCard = userCard_Frame(self)
-        self.userCard.grid(row=1, column=1, padx=10, pady=(10,10), sticky="ew")
+        self.userCard.grid(row=1, column=1, padx=(0,10), pady=(10,10), sticky="ew")
 
         self.statusWidget = status_Frame(self)
-        self.statusWidget.grid(row=2, column=1, padx=10, pady=(0,10),sticky="ew")
+        self.statusWidget.grid(row=2, column=1, padx=(0,10), pady=(0,10),sticky="ew")
 
         self.timeCard = timeCard_Frame(self)
         self.timeCard.grid(row=1, column=0, padx=10,pady=(10,10),sticky="nsew", rowspan=2)
---
+++ Server/GUI.py
@@ -0,0 +1,166 @@
+import customtkinter
+import random
+from datetime import datetime, timedelta
+
+user = "Admin"
+
+ # STATUS NEEDS ADMIN OR BACKEND INTEGRATION
+ # |
+ # v
+
+status = True
+
+date_and_time = datetime.now()
+time = date_and_time.strftime("%H:%M")
+date = date_and_time.strftime("%d/%m/%Y")
+
+def get_date (days_ago):
+    today = datetime.now()
+    target_date = today - timedelta(days=days_ago)
+    return target_date.strftime("%d/%m/%Y")
+
+def get_error (error_num):
+    return "#" + str(error_num) + " Error"
+
+def get_status (boolean):
+    if boolean == True:
+        return "ONLINE"
+    else:
+        return "OFFLINE"
+
+def report_click ():
+    global status
+    if status == True:
+        status = False
+    else:
+        status = True
+
+class errorLog_Frame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        # Widgets
+        self.errorLabel1 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text_color="#FF2222", text=get_error(1))
+        self.errorLabel1.grid(row=0, column=0, padx=20, pady=(8), sticky="nw")
+        
+        self.errorLabel2 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text_color="#FF2222", text=get_error(2))
+        self.errorLabel2.grid(row=1, column=0, padx=20, pady=(8), sticky="nw")
+
+        self.errorLabel3 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text_color="#FF2222", text=get_error(3))
+        self.errorLabel3.grid(row=2, column=0, padx=20, pady=(8), sticky="nw")
+        
+        self.errorLabel4 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text_color="#FF2222", text=get_error(4))
+        self.errorLabel4.grid(row=3, column=0, padx=20, pady=(8), sticky="nw")
+
+        self.errorLabel5 = customtkinter.CTkLabel(self, font=("Consolas" ,11), text_color="#FF2222", text=get_error(5))
+        self.errorLabel5.grid(row=4, column=0, padx=20, pady=(8), sticky="nw")
+
+class userCard_Frame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.loggedInLabel = customtkinter.CTkLabel(
+            self,
+            font=("Consolas", 10),
+            text_color="#A3A3A3",
+            text="You are logged in as:",
+            anchor="w"
+        )
+        self.loggedInLabel.grid(row=0, column=0, padx=20, pady=(15,0), sticky="nsew")
+
+        self.nameLabel = customtkinter.CTkLabel(
+            self,
+            font=("Consolas", 14),
+            text_color="#FFFFF0",
+            text=user,
+            anchor="w"
+        )
+        self.nameLabel.grid(row=1, column=0, padx=20, pady=(0, 15), sticky="nsew")
+
+        #self.profilePicture = customtkinter.CTkImage("Icon2.ico")
+        #self.profilePicture.grid(row=0, column=1, padx=20, pady=(15,0), sticky="e")
+
+class status_Frame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.statusLabel = customtkinter.CTkLabel(
+            self,
+            font=("Consolas", 12),
+            text_color="#FFFFF0",
+            text="Powergrid Status: " + get_status(status),
+            anchor="w"
+        )
+        self.statusLabel.grid(row=0, column=0, padx=20, pady=(10, 10), sticky="nsew")
+
+        self.update_status()
+
+    def update_status(self):
+        self.statusLabel.configure(text="Powergrid Status: " + get_status(status))
+        self.after(1000, self.update_status)
+
+class button_Frame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.reportButton = customtkinter.CTkButton(
+            self,
+            text="Report Issue",
+            font=("Consolas", 12),
+            text_color="#FFFFF0",
+            command=report_click
+        )
+        self.reportButton.grid(row=0, column=0, padx=20, pady=(10, 10), sticky="nsew")
+
+        self.update_status()
+
+    def get_button_text(self):
+        if status == True:
+            return "Report Issue"
+        else:
+            return "Report Fixed"
+        
+    def update_status(self):
+        self.reportButton.configure(text=self.get_button_text())
+        self.after(1000, self.update_status)
+
+class mainWindow(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("616x254")
+        self.title("Smart Meter: Admin")
+        self.grid_columnconfigure(0, weight=3)
+        self.grid_columnconfigure(1, weight=2)
+        self.grid_rowconfigure(0, weight=3)
+        self.grid_rowconfigure(1, weight=1)
+        self.resizable(False, False)
+
+        self.errorLog = errorLog_Frame(self)
+        self.errorLog.grid(row=0, column=0, rowspan=3, padx=10, pady=(10), sticky="nsew")
+
+        self.userCard = userCard_Frame(self)
+        self.userCard.grid(row=0, column=1, padx=(0,10), pady=(10,10), sticky="nsew")
+
+        self.statusWidget = status_Frame(self)
+        self.statusWidget.grid(row=1, column=1, padx=(0,10), pady=(0,10), sticky="nsew")
+
+        self.buttonWidget = button_Frame(self)
+        self.buttonWidget.grid(row=2, column=1, padx=(0,10), pady=(0,10), sticky="nsew")
+
+
+    # Add Methods to app
+    def button_callback(self):
+        print("Text output")
+
+mainWindow = mainWindow()
+mainWindow.mainloop()
